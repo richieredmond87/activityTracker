7f148fc2f0624e04384b6ff1d27da24791f321b5
richieredmond87 <richieredmond87@gmail.com>
2020-11-04 18:05:32 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 2,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
