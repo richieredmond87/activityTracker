16ed66afbd1290c6e971c7cb35be6e566ed95770
richieredmond87 <richieredmond87@gmail.com>
2020-11-04 18:25:16 +0000

    Add categories dropdown

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def logout():
 
 @app.route("/add_activity")
 def add_activity():
-    return render_template("add_activity.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_activity.html", categories=categories)
 
 
 if __name__ == "__main__":
