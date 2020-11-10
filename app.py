d4c5cf858a468f35f1baf0cca56e761da39108ae
richieredmond87 <richieredmond87@gmail.com>
2020-11-10 10:10:34 +0000

    Wire up activity edit button

--- app.py
+++ app.py
@@ -119,6 +119,13 @@ def add_activity():
     return render_template("add_activity.html", categories=categories)
 
 
+@app.route("/edit_activity/<activity_id>", methods=["GET", "POST"])
+def edit_activity(activity_id):
+    activity = mongo.db.activity.find_one({"_id": ObjectId(activity_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_activity.html", activity=activity, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
